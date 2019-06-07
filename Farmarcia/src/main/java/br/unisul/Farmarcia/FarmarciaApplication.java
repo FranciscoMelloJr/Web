0dc2ada62fa797f305609d42b260588bdfb3ad3e
FranciscoMelloJr <franciscomelloxpe@gmail.com>
2019-06-07 20:42:12 -0300

    Projeto Sexta
    Inicio Mavem

---
+++ Farmarcia/src/main/java/br/unisul/Farmarcia/FarmarciaApplication.java
@@ -0,0 +1,13 @@
+package br.unisul.Farmarcia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmarciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmarciaApplication.class, args);
+	}
+
+}
---
+++ Farmarcia/src/test/java/br/unisul/Farmarcia/FarmarciaApplicationTests.java
@@ -0,0 +1,16 @@
+package br.unisul.Farmarcia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FarmarciaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
