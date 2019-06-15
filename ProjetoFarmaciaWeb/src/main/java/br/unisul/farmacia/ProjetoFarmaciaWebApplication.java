c2551b7a6d8a944dc6e09363441cde4bc8719dbd
FranciscoMelloJr <franciscomelloxpe@gmail.com>
2019-06-15 20:01:12 -0300

    2.1.3

--- ProjetoFarmaciaWeb/src/main/java/br/unisul/farmacia/ProjetoFarmaciaWebApplication.java
+++ ProjetoFarmaciaWeb/src/main/java/br/unisul/farmacia/ProjetoFarmaciaWebApplication.java
@@ -1,13 +1,13 @@
-package br.unisul.Farmarcia;
+package br.unisul.farmacia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FarmarciaApplication {
+public class ProjetoFarmaciaWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FarmarciaApplication.class, args);
+		SpringApplication.run(ProjetoFarmaciaWebApplication.class, args);
 	}
 
 }
--- ProjetoFarmaciaWeb/src/test/java/br/unisul/farmacia/ProjetoFarmaciaWebApplicationTests.java
+++ ProjetoFarmaciaWeb/src/test/java/br/unisul/farmacia/ProjetoFarmaciaWebApplicationTests.java
@@ -1,4 +1,4 @@
-package br.unisul.Farmarcia;
+package br.unisul.farmacia;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FarmarciaApplicationTests {
+public class ProjetoFarmaciaWebApplicationTests {
 
 	@Test
 	public void contextLoads() {
