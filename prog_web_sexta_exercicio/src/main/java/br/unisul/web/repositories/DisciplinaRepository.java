bfa45fdaf4671557bf64c97d886d507d1999d1c5
FranciscoMelloJr <franciscomelloxpe@gmail.com>
2019-06-28 20:14:07 -0300

    att

--- prog_web_sexta_exercicio/src/main/java/br/unisul/web/repositories/DisciplinaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.unisul.web.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.unisul.web.domain.Disciplina;
-
-public interface DisciplinaRepository extends JpaRepository<Disciplina, Integer> {
-
-}
